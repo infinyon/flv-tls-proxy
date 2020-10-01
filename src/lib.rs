1b2d239e1a9fdd6cd646db0699ab5016c117a6c0
Sehyo Chang <sehyo@fluvio.io>
2020-09-30 21:17:37 -0700

    add test for proxy (#2)
    * update
    
    * recrate io copy in order to add debug log
    use zip to run both stream concurrently
    
    * update repo info
    
    * add CI and update makefile
    
    * apply clippy and fmt
    
    * remove assert true
    
    * fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,29 @@
 use std::io::Error as IoError;
 
-use log::debug;
-use log::info;
-use log::error;
-use futures_util::stream::StreamExt;
-use futures_util::io::AsyncReadExt;
-use futures_lite::io::copy;
 use fluvio_future::net::TcpListener;
 use fluvio_future::net::TcpStream;
-use fluvio_future::tls::TlsAcceptor;
-use fluvio_future::tls::DefaultServerTlsStream;
 use fluvio_future::task::spawn;
+use fluvio_future::tls::DefaultServerTlsStream;
+use fluvio_future::tls::TlsAcceptor;
+use futures_lite::io::copy;
 
+use futures_util::io::AsyncReadExt;
+use futures_util::stream::StreamExt;
+use log::debug;
+use log::error;
+use log::info;
 
 /// start TLS proxy at addr to target
-pub async fn start(addr: &str, acceptor: TlsAcceptor, target: String) -> Result<(),IoError> {
-
+pub async fn start(addr: &str, acceptor: TlsAcceptor, target: String) -> Result<(), IoError> {
     let listener = TcpListener::bind(addr).await?;
-    info!("proxy started at: {}",addr);
+    info!("proxy started at: {}", addr);
     let mut incoming = listener.incoming();
     while let Some(stream) = incoming.next().await {
-        
         debug!("server: got connection from client");
         if let Ok(tcp_stream) = stream {
-
-            debug!(
-                "new connection from {}",
-                tcp_stream
-                    .peer_addr()
-                    .map(|addr| addr.to_string())
-                    .unwrap_or("".to_owned())
-            );
-            
-            let handshake = acceptor.accept(tcp_stream);
-
-            match handshake.await {
-                Ok(inner_stream) => {
-                    debug!("handshake success: starting");
-                    process_stream(inner_stream,target.clone()).await;
-                },
-                Err(err) => error!("error handshaking: {}",err)
-            }
+            spawn(process_stream(acceptor.clone(), tcp_stream, target.clone()));
+        } else {
+            error!("no stream detected");
         }
     }
 
@@ -48,52 +31,149 @@ pub async fn start(addr: &str, acceptor: TlsAcceptor, target: String) -> Result<
     Ok(())
 }
 
+async fn process_stream(acceptor: TlsAcceptor, raw_stream: TcpStream, target: String) {
+    let source = raw_stream
+        .peer_addr()
+        .map(|addr| addr.to_string())
+        .unwrap_or_else(|_| "".to_owned());
 
-async fn process_stream(stream: DefaultServerTlsStream, target: String) {
+    debug!("new connection from {}", source);
 
-    // connect to other end
-    if let Err(err) = proxy(stream,target).await {
-        error!("error processing tls: {}",err);
+    let handshake = acceptor.accept(raw_stream);
+
+    match handshake.await {
+        Ok(inner_stream) => {
+            debug!("handshake success from: {}", source);
+            if let Err(err) = proxy(inner_stream, target, source.clone()).await {
+                error!("error processing tls: {} from source: {}", err, source);
+            }
+        }
+        Err(err) => error!("error handshaking: {} from source: {}", err, source),
     }
-    
 }
 
-async fn proxy(tls_stream: DefaultServerTlsStream, target: String) -> Result<(),IoError> {
+async fn proxy(
+    tls_stream: DefaultServerTlsStream,
+    target: String,
+    source: String,
+) -> Result<(), IoError> {
+    use futures_lite::future::zip;
 
-    debug!("trying to connect to target at: {}", target);
-    let tcp_stream = TcpStream::connect(&target).await?;
+    debug!(
+        "trying to connect to target at: {} from source: {}",
+        target, source
+    );
+    let mut tcp_stream = TcpStream::connect(&target).await?;
 
-    debug!("connect to target: {}",target);
-    let (mut target_stream,mut target_sink) = tcp_stream.split();
-    let (mut from_tls_stream,mut from_tls_sink) = tls_stream.split();
-    
-    let source_to_target_ft = async move {
+    debug!("connect to target: {} from source: {}", target, source);
+    let mut target_sink = tcp_stream.clone();
 
-        match copy(&mut from_tls_stream,&mut target_sink).await {
-            Ok(_) => {
-                debug!("done copying from source to target");
-            },
+    //let (mut target_stream, mut target_sink) = tcp_stream.split();
+    let (mut from_tls_stream, mut from_tls_sink) = tls_stream.split();
+
+    let s_t = format!("{}->{}", source, target);
+    let t_s = format!("{}->{}", target, source);
+    let source_to_target_ft = async {
+        match copy(&mut from_tls_stream, &mut target_sink).await {
+            Ok(len) => {
+                debug!("{} copy from source to target: len {}", s_t, len);
+            }
             Err(err) => {
-                error!("error copying: {}",err);
+                error!("{} error copying: {}", s_t, err);
             }
         }
     };
 
-    let target_to_source = async move {
-        match copy(&mut target_stream,&mut from_tls_sink).await {
-            Ok(_) => {
-                debug!("done copying from source to target");
-            },
+    let target_to_source = async {
+        match copy(&mut tcp_stream, &mut from_tls_sink).await {
+            Ok(len) => {
+                debug!("{} copy from target: len {}", t_s, len);
+            }
             Err(err) => {
-                error!("error copying: {}",err);
+                error!("{} error copying: {}", t_s, err);
             }
         }
     };
 
-    spawn(source_to_target_ft);
-    spawn(target_to_source);
+    zip(source_to_target_ft, target_to_source).await;
 
     Ok(())
 }
 
-    
+/*
+mod copy {
+
+    use std::io;
+    use std::pin::Pin;
+
+    use core::task::{Context, Poll};
+    use log::trace;
+    use pin_project_lite::pin_project;
+
+    use futures_util::future::Future;
+    use futures_util::io::AsyncBufRead as BufRead;
+    use futures_util::io::AsyncRead as Read;
+    use futures_util::io::AsyncWrite as Write;
+    use futures_util::io::BufReader;
+    use futures_util::ready;
+
+    pub async fn copy<R, W>(reader: &mut R, writer: &mut W, label: String) -> io::Result<u64>
+    where
+        R: Read + Unpin + ?Sized,
+        W: Write + Unpin + ?Sized,
+    {
+        pin_project! {
+            struct CopyFuture<R, W> {
+                #[pin]
+                reader: R,
+                #[pin]
+                writer: W,
+                amt: u64,
+                #[pin]
+                label: String
+            }
+        }
+
+        impl<R, W> Future for CopyFuture<R, W>
+        where
+            R: BufRead,
+            W: Write + Unpin,
+        {
+            type Output = io::Result<u64>;
+
+            fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+                let mut this = self.project();
+
+                loop {
+                    trace!("{}, starting loop with amt {}", this.label, this.amt);
+                    let buffer = ready!(this.reader.as_mut().poll_fill_buf(cx))?;
+                    if buffer.is_empty() {
+                        trace!("{}, buffer is empty, flushing", this.label);
+                        ready!(this.writer.as_mut().poll_flush(cx))?;
+                        return Poll::Ready(Ok(*this.amt));
+                    }
+
+                    trace!("{}, read {}", this.label, buffer.len());
+                    let i = ready!(this.writer.as_mut().poll_write(cx, buffer))?;
+                    trace!("{}, write: {}", this.label, i);
+                    if i == 0 {
+                        trace!("{}, no write occur returning", this.label);
+                        return Poll::Ready(Err(io::ErrorKind::WriteZero.into()));
+                    }
+                    *this.amt += i as u64;
+                    trace!("{},consuming amt: {}", this.label, this.amt);
+                    this.reader.as_mut().consume(i);
+                }
+            }
+        }
+
+        let future = CopyFuture {
+            reader: BufReader::new(reader),
+            writer,
+            amt: 0,
+            label,
+        };
+        future.await
+    }
+}
+*/
---
+++ tests/proxy.rs
@@ -0,0 +1,164 @@
+use std::io::Error as IoError;
+use std::net::SocketAddr;
+use std::time;
+
+use bytes::BufMut;
+use bytes::Bytes;
+use bytes::BytesMut;
+use log::debug;
+
+use futures_lite::future::zip;
+use futures_lite::stream::StreamExt;
+use futures_util::sink::SinkExt;
+use tokio_util::codec::BytesCodec;
+use tokio_util::codec::Framed;
+use tokio_util::compat::FuturesAsyncReadCompatExt;
+
+use fluvio_future::tls::TlsAcceptor;
+use fluvio_future::tls::TlsConnector;
+
+use fluvio_future::net::TcpListener;
+use fluvio_future::net::TcpStream;
+use fluvio_future::test_async;
+use fluvio_future::timer::sleep;
+
+use fluvio_future::tls::AcceptorBuilder;
+use fluvio_future::tls::AllTcpStream;
+use fluvio_future::tls::ConnectorBuilder;
+
+use flv_tls_proxy::start;
+
+// const CA_PATH: &'static str = "certs/certs/ca.crt";
+
+const SERVER: &str = "127.0.0.1:19998";
+const PROXY: &str = "127.0.0.1:20000";
+const ITER: u16 = 10;
+
+fn to_bytes(bytes: &[u8]) -> Bytes {
+    let mut buf = BytesMut::with_capacity(bytes.len());
+    buf.put_slice(&bytes);
+    buf.freeze()
+}
+
+#[test_async]
+async fn test_proxy() -> Result<(), IoError> {
+    test_tls(
+        AcceptorBuilder::new_no_client_authentication()
+            .load_server_certs("certs/certs/server.crt", "certs/certs/server.key")?
+            .build(),
+        ConnectorBuilder::new().no_cert_verification().build(),
+    )
+    .await
+    .expect("no client cert test failed");
+
+    // test client authentication
+    /*
+    test_tls(
+        AcceptorBuilder::new_client_authenticate(CA_PATH)?
+            .load_server_certs("certs/certs/server.crt", "certs/certs/server.key")?
+            .build(),
+        ConnectorBuilder::new()
+            .load_client_certs("certs/certs/client.crt", "certs/certs/client.key")?
+            .load_ca_cert(CA_PATH)?
+            .build(),
+    )
+    .await
+    .expect("client cert test fail");
+    */
+
+    Ok(())
+}
+
+async fn test_tls(acceptor: TlsAcceptor, connector: TlsConnector) -> Result<(), IoError> {
+    let addr = SERVER.parse::<SocketAddr>().expect("parse");
+
+    let server_ft = async {
+        debug!("server: binding");
+        let listener = TcpListener::bind(&addr).await.expect("listener failed");
+        debug!("server: successfully binding. waiting for incoming");
+
+        let mut incoming = listener.incoming();
+        while let Some(stream) = incoming.next().await {
+            debug!("server: new incoming connection");
+
+            let tcp_stream = stream.expect("no stream");
+
+            let mut framed = Framed::new(tcp_stream.compat(), BytesCodec::new());
+
+            let mut i: u16 = 0;
+            if let Some(value) = framed.next().await {
+                let bytes = value.expect("invalid value");
+                debug!(
+                    "server: loop {}, received from client: {} bytes",
+                    i,
+                    bytes.len()
+                );
+
+                let slice = bytes.as_ref();
+                let mut str_bytes = vec![];
+                for b in slice {
+                    str_bytes.push(b.to_owned());
+                }
+                let message = String::from_utf8(str_bytes).expect("utf8");
+                assert_eq!(message, format!("message{}", i));
+                let resply = format!("{}reply", message);
+                let reply_bytes = resply.as_bytes();
+                debug!("sever: send back reply: {}", resply);
+                framed
+                    .send(to_bytes(reply_bytes))
+                    .await
+                    .expect("send failed");
+
+                assert!(i < ITER);
+
+                i += 1;
+                if i == ITER {
+                    return Ok(()) as Result<(), IoError>;
+                }
+            }
+        }
+
+        Ok(()) as Result<(), IoError>
+    };
+
+    let client_ft = async {
+        debug!("client: sleep to give server chance to come up");
+        sleep(time::Duration::from_millis(200)).await;
+        debug!("client: trying to connect");
+        let tcp_stream = TcpStream::connect(PROXY.to_owned())
+            .await
+            .expect("connection fail");
+        let tls_stream = connector
+            .connect("localhost", tcp_stream)
+            .await
+            .expect("tls failed");
+        let all_stream = AllTcpStream::Tls(tls_stream);
+        let mut framed = Framed::new(all_stream.compat(), BytesCodec::new());
+        debug!("client: got connection. waiting");
+
+        // do loop for const
+        for i in 0..ITER {
+            let message = format!("message{}", i);
+            let bytes = message.as_bytes();
+            debug!("client: loop {} sending test message", i);
+            framed.send(to_bytes(bytes)).await.expect("send failed");
+            let reply = framed.next().await.expect("messages").expect("frame");
+            debug!("client: loop {}, received reply back", i);
+            let slice = reply.as_ref();
+            let mut str_bytes = vec![];
+            for b in slice {
+                str_bytes.push(b.to_owned());
+            }
+            let message = String::from_utf8(str_bytes).expect("utf8");
+            assert_eq!(message, format!("message{}reply", i));
+        }
+
+        Ok(()) as Result<(), IoError>
+    };
+
+    let proxy = start(PROXY, acceptor.clone(), SERVER.to_string());
+
+    let _ = zip(proxy, zip(client_ft, server_ft)).await;
+
+    Ok(())
+}
