f4e2d536bb4f3e762c0dbc7db9d99951cf8ef32d
Sehyo <sehyo@fluvio.io>
2020-10-01 16:47:30 +0000

    add async-net as dev dependencies copy proxy code in the tes

--- tests/proxy.rs
+++ tests/proxy.rs
@@ -4,23 +4,23 @@ use std::time;
 
 use log::debug;
 
+
 use futures_lite::future::zip;
-use futures_lite::stream::StreamExt;
 use futures_lite::AsyncReadExt;
 use futures_lite::AsyncWriteExt;
+use futures_util::stream::StreamExt;
+
+use async_net::TcpListener;
+use async_net::TcpStream;
 
 use fluvio_future::tls::TlsAcceptor;
 use fluvio_future::tls::TlsConnector;
-
-use fluvio_future::net::TcpListener;
-use fluvio_future::net::TcpStream;
 use fluvio_future::test_async;
 use fluvio_future::timer::sleep;
 
 use fluvio_future::tls::AcceptorBuilder;
 use fluvio_future::tls::ConnectorBuilder;
 
-use flv_tls_proxy::start;
 
 // const CA_PATH: &'static str = "certs/certs/ca.crt";
 
@@ -136,9 +136,116 @@ async fn test_tls(acceptor: TlsAcceptor, connector: TlsConnector) -> Result<(), 
         Ok(()) as Result<(), IoError>
     };
 
-    let proxy = start(PROXY, acceptor.clone(), SERVER.to_string());
+    let proxy = proxy::start(PROXY, acceptor.clone(), SERVER.to_string());
 
     let _ = zip(proxy, zip(client_ft, server_ft)).await;
 
     Ok(())
+}
+
+
+mod proxy {
+
+    use std::io::Error as IoError;
+
+    use async_net::TcpListener;
+    use async_net::TcpStream;
+    use fluvio_future::task::spawn;
+    use fluvio_future::tls::DefaultServerTlsStream;
+    use fluvio_future::tls::TlsAcceptor;
+    use futures_lite::io::copy;
+
+    use futures_util::io::AsyncReadExt;
+    use futures_util::stream::StreamExt;
+    use log::debug;
+    use log::error;
+    use log::info;
+
+    /// start TLS proxy at addr to target
+    pub async fn start(addr: &str, acceptor: TlsAcceptor, target: String) -> Result<(), IoError> {
+        let listener = TcpListener::bind(addr).await?;
+        info!("proxy started at: {}", addr);
+        let mut incoming = listener.incoming();
+        while let Some(stream) = incoming.next().await {
+            debug!("server: got connection from client");
+            if let Ok(tcp_stream) = stream {
+                spawn(process_stream(acceptor.clone(), tcp_stream, target.clone()));
+            } else {
+                error!("no stream detected");
+            }
+        }
+
+        info!("server terminated");
+        Ok(())
+    }
+
+    async fn process_stream(acceptor: TlsAcceptor, raw_stream: TcpStream, target: String) {
+        let source = raw_stream
+            .peer_addr()
+            .map(|addr| addr.to_string())
+            .unwrap_or_else(|_| "".to_owned());
+
+        debug!("new connection from {}", source);
+
+        let handshake = acceptor.accept(raw_stream);
+
+        match handshake.await {
+            Ok(inner_stream) => {
+                debug!("handshake success from: {}", source);
+                if let Err(err) = proxy(inner_stream, target, source.clone()).await {
+                    error!("error processing tls: {} from source: {}", err, source);
+                }
+            }
+            Err(err) => error!("error handshaking: {} from source: {}", err, source),
+        }
+    }
+
+    async fn proxy(
+        tls_stream: DefaultServerTlsStream,
+        target: String,
+        source: String,
+    ) -> Result<(), IoError> {
+        use futures_lite::future::zip;
+
+        debug!(
+            "trying to connect to target at: {} from source: {}",
+            target, source
+        );
+        let mut tcp_stream = TcpStream::connect(&target).await?;
+
+        debug!("connect to target: {} from source: {}", target, source);
+        let mut target_sink = tcp_stream.clone();
+
+        //let (mut target_stream, mut target_sink) = tcp_stream.split();
+        let (mut from_tls_stream, mut from_tls_sink) = tls_stream.split();
+
+        let s_t = format!("{}->{}", source, target);
+        let t_s = format!("{}->{}", target, source);
+        let source_to_target_ft = async {
+            match copy(&mut from_tls_stream, &mut target_sink).await {
+                Ok(len) => {
+                    debug!("{} copy from source to target: len {}", s_t, len);
+                }
+                Err(err) => {
+                    error!("{} error copying: {}", s_t, err);
+                }
+            }
+        };
+
+        let target_to_source = async {
+            match copy(&mut tcp_stream, &mut from_tls_sink).await {
+                Ok(len) => {
+                    debug!("{} copy from target: len {}", t_s, len);
+                }
+                Err(err) => {
+                    error!("{} error copying: {}", t_s, err);
+                }
+            }
+        };
+
+        zip(source_to_target_ft, target_to_source).await;
+
+        Ok(())
+    }
+
 }
